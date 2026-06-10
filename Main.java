d287a426f643813a6e73c9f1751e5a89db599d7a
Akhil Rai <raiakhil2627@gmail.com>
2026-06-10 12:49:09 +0530

    [akhil] added : assignment of control flow and programming elements

---
+++ Main.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class Main{
+	public static void main(String  args[]){
+		ArrayList a= new ArrayList();
+		a.add(1);
+		a.add("ABC");
+		a.add(1.222);
+		a.add('A');
+		System.out.print(a);
+	}
+}	
